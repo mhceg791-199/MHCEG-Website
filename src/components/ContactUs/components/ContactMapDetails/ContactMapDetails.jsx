26e29542bdcea9ed43bc57f4cb4ab94e67c6fd98
mhceg791-199 <mhceg791@gmail.com>
2025-11-27 13:06:34 +0200

    updated

--- src/components/ContactUs/components/ContactMapDetails/ContactMapDetails.jsx
+++ src/components/ContactUs/components/ContactMapDetails/ContactMapDetails.jsx
@@ -2,7 +2,7 @@ import map from "../../../../assets/world-map-gold.jpg";
 
 function ContactMapDetails() {
   return (
-    <div className=" w-full bg-mainColor text-white rounded-b-sm overflow-hidden shadow-lg border border-mainGold/30">
+    <div className="w-full bg-mainColor text-white rounded-b-sm overflow-hidden shadow-lg border border-mainGold/30">
       <div className="relative w-full border-8 bg-mainColor border-mainColor">
         <img src={map} alt="map" className="w-full object-cover h-64 md:h-72" />
         <div className="absolute inset-0 bg-mainColor/50 to-transparent pointer-events-none"></div>
@@ -35,13 +35,38 @@ function ContactMapDetails() {
           <div>
             <h4 className="text-lg font-semibold">Do you have a Question?</h4>
             <a
-              href="mailto:info@mosaicholding.com"
+              href="mailto:info@mhceg.com"
               className="text-gray-400 text-md hover:text-mainGold transition-colors duration-300"
             >
               info@mhceg.com
             </a>
           </div>
         </div>
+
+        {/* Phones */}
+        <div className="flex items-start gap-4 mt-5">
+          <i className="fa-solid fa-phone text-mainGold text-2xl mt-1"></i>
+          <div>
+            <h4 className="text-lg font-semibold">Phone</h4>
+
+            <div className="flex flex-col text-gray-400 text-md">
+              <a
+                href="tel:0224190150"
+                className="hover:text-mainGold transition-colors duration-300"
+              >
+                02 24190150
+              </a>
+
+              <a
+                href="tel:0224190119"
+                className="hover:text-mainGold transition-colors duration-300"
+              >
+                02 24190119
+              </a>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
